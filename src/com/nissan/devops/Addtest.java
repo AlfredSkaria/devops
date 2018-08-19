db4d993e0cff1b622aeed0d9a5262cb9ba635885
AlfredSkaria <alfiskaria@gmail.com>
2018-08-19 14:07:41 +0530

    c4

--- src/com/nissan/devops/Addtest.java
+++ src/com/nissan/devops/Addtest.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 public class Addtest {
 
 	@Test
-	public void test() {
+	public void testadd() {
 		Calculate c= new Calculate();
-		assertEquals(10,c.add(5, 5));
+		assertEquals(10,c.add(5, 5),0);
 	}
 
 }
